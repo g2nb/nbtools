a37ecbe8f19d35412ef6b6d507da203398d03466
tmtabor <tabor@broadinstitute.org>
2017-06-08 13:14:57 -0700

    Updating to version 0.1.4

--- setup.py
+++ setup.py
@@ -39,13 +39,13 @@ class NBDevelop(_develop):
 
 setup(name='nbtools',
       packages=['nbtools'],
-      version='0.1.3',
+      version='0.1.4',
       description='A lightweight manager for registering and browsing Jupyter tools',
       license='BSD',
       author='Thorin Tabor',
       author_email='thorin@broadinstitute.org',
       url='https://github.com/genepattern/nbtool-manager',
-      download_url='https://github.com/genepattern/nbtool-manager/archive/0.1.3.tar.gz',
+      download_url='https://github.com/genepattern/nbtool-manager/archive/0.1.4.tar.gz',
       keywords=['genepattern', 'genomics', 'bioinformatics', 'ipython', 'jupyter'],
       classifiers=[
           'Development Status :: 4 - Beta',
