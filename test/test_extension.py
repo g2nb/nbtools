4a2f08f9536bb017cb5addefac8d4a234e891c73
tmtabor <tabor@broadinstitute.org>
2018-04-02 15:49:56 -0400

    Added import test

--- test/test_extension.py
+++ test/test_extension.py
@@ -2,9 +2,8 @@
 Tests for importing the kernel-side widgets
 """
 import pytest
-import nbtools
 
 
-def test_list():
-    tools = nbtools.list()
-    assert tools is not None
+def test_import():
+    import nbtools
+    assert True
