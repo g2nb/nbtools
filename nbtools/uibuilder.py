c946262539936d8354626ba3ac1da5f93d0478c6
tmtabor <tabor@broadinstitute.org>
2019-11-01 15:52:39 -0700

    Display test parameters with name and description (work in progress)

--- nbtools/uibuilder.py
+++ nbtools/uibuilder.py
@@ -3,9 +3,12 @@ import inspect
 import functools
 import re
 import urllib.request
+from numbers import Integral, Real
+
 from IPython.core.display import display
 from traitlets import Unicode, List, Bool, Dict, Instance
-from ipywidgets import DOMWidget, interactive, widget_serialization, Output
+from ipython_genutils.py3compat import string_types, unicode_type
+from ipywidgets import DOMWidget, interactive, widget_serialization, Output, Text, Checkbox, IntSlider, FloatSlider, GridBox, Label, Layout, ValueWidget
 from ._frontend import module_name, module_version
 
 
@@ -99,15 +102,82 @@ class build_ui:
         display(self.__widget__)
 
 
-class UIForm(interactive):
-    def __init__(self, function_or_method, **kwargs):
+class TextFormInput(GridBox, ValueWidget):
+    label = Label(layout=Layout(width='auto', grid_area='label'))
+    input = Text(layout=Layout(width='auto', grid_area='input'))
+    description = Label(layout=Layout(width='auto', grid_area='description'))
+
+    def __init__(self, spec, **kwargs):
+        self._apply_spec(spec)
+        GridBox.__init__(self, [self.label, self.input, self.description], layout=Layout(
+                width='100%',
+                grid_template_rows='auto auto',
+                grid_template_columns='25% 75%',
+                grid_template_areas='''
+                    "label input"
+                    ". description"
+                '''), **kwargs)
+
+    @property
+    def value(self):
+        return self.input.value
+
+    @value.setter
+    def value(self, val):
+        self.input.value = val
+
+    def _apply_spec(self, spec):
+        """Apply the parameter spec to the widget"""
+        # TODO: Handle other parameter attributes like hide=True here
+        # Set self.label.description=spec.name etc.?
+        self.label.value = spec['label']
+        self.label.description = spec['label']
+
+        self.input.value = spec['default']
+
+        self.description.value = spec['description']
+        self.description.description = spec['description']
+
+
+class InteractiveForm(interactive):
+    def __init__(self, function_or_method, parameter_specs, **kwargs):
+        # Create parameter widgets from spec and add to kwargs
+        self.widgets_from_spec(parameter_specs, kwargs)
+
+        # Call the superclass constructor
         interactive.__init__(self, function_or_method, {
             'manual': True,
             'manual_name': 'Run',
             'auto_display': False
         }, **kwargs)
 
+        # Don't display the output as a child widget
         self.children = self.children[:len(self.children)-1]
+
+    def widgets_from_spec(self, parameter_specs, kwargs):
+        """Iterate over each parameter spec and create a form widget"""
+        for spec in parameter_specs:
+            param_name = spec['name']
+            kwargs[param_name] = self.widget_from_spec(spec)
+
+    @staticmethod
+    def widget_from_spec(spec):
+        """Instantiate a widget based on the default value in the spec"""
+        default_value = spec['default']
+        # TODO: Handle type override
+
+        if isinstance(default_value, string_types):
+            return TextFormInput(spec, value=unicode_type(default_value))
+        elif isinstance(default_value, bool):
+            return Checkbox(value=default_value)
+        elif isinstance(default_value, Integral):
+            min, max, value = super._get_min_max_value(None, None, default_value)
+            return IntSlider(value=default_value, min=min, max=max)
+        elif isinstance(default_value, Real):
+            min, max, value = super._get_min_max_value(None, None, default_value)
+            return FloatSlider(value=default_value, min=min, max=max)
+        else:
+            return Text(value=unicode_type(default_value))
 
 
 class UIBuilder(DOMWidget):
@@ -132,7 +202,7 @@ class UIBuilder(DOMWidget):
     register_tool = Bool(sync=True)
     collapse = Bool(sync=True)
     events = Dict(sync=True)
-    form = Instance(interactive, (None, )).tag(sync=True, **widget_serialization)
+    form = Instance(InteractiveForm, (None, [])).tag(sync=True, **widget_serialization)
     output = Instance(Output, ()).tag(sync=True, **widget_serialization)
     function_or_method = None
 
@@ -149,8 +219,7 @@ class UIBuilder(DOMWidget):
             else: setattr(self, key, value)
 
         # Create the form and output child widgets
-        self.form = interactive(function_or_method, { 'manual': True, 'manual_name': 'Run', 'auto_display': False, })
-        self.form.children = self.form.children[:len(self.form.children) - 1]  # Don't display output in the form
+        self.form = InteractiveForm(function_or_method, self.parameters)
         self.output = self.form.out
 
         # Display the output underneath the UI Builder widget
@@ -185,6 +254,7 @@ class UIBuilder(DOMWidget):
                 "label": param.name,
                 "optional": param.default != inspect.Signature.empty,
                 "default": UIBuilder._safe_default(param.default),
+                "value": UIBuilder._safe_default(param.default),
                 "description": param.annotation if param.annotation != inspect.Signature.empty else '',
                 "hide": False,
                 "type": UIBuilder._guess_type(param.default),
@@ -231,218 +301,3 @@ class UIBuilder(DOMWidget):
         # TODO: Handle enums here in the future
         else:
             return None
-
-
-        # TODO: Old code below this -- UPDATE IT!
-    #     import nbtools
-    #
-    #     # Read call signature
-    #     sig = inspect.signature(function_or_method)
-    #
-    #     # Read params, values and annotations from the signature
-    #     params = self._params(sig)
-    #
-    #     # Read docstring
-    #     docstring = self._docstring(function_or_method)
-    #
-    #     # Do arguments override the default name, description or output?
-    #     custom_name = kwargs['name'] if 'name' in kwargs else None
-    #     custom_desc = kwargs['description'] if 'description' in kwargs else None
-    #     custom_output = kwargs['output_var'] if 'output_var' in kwargs else None
-    #     custom_origin = kwargs['origin'] if 'origin' in kwargs else self._determine_origin(function_or_method)
-    #     custom_import = kwargs['function_import'] if 'function_import' in kwargs else None
-    #     custom_register = kwargs['register_tool'] if 'register_tool' in kwargs else True
-    #     custom_collapse = kwargs['collapse'] if 'collapse' in kwargs else True
-    #
-    #     # Apply output_var deprecation warning
-    #     if 'output_var' in kwargs:
-    #         print('Specifying the output variable using the output_var argument is deprecated. Use the parameters argument.')
-    #
-    #     # Read parameter metadata
-    #     if 'parameters' in kwargs:
-    #         self._apply_custom_parameter_info(params, kwargs['parameters'])
-    #
-    #     # Set the Traitlet values for the call
-    #     self.name = custom_name or function_or_method.__qualname__
-    #     self.description = custom_desc or docstring
-    #     self.output_var = custom_output or ''
-    #     self.origin = custom_origin or 'Notebook'
-    #     self.params = params
-    #     self.function_import = custom_import or self._import(function_or_method)
-    #     self.register_tool = custom_register
-    #     self.collapse = custom_collapse
-    #     self.events = self.events
-    #     self.function_or_method = function_or_method
-    #
-    #     # Add widget reference to function
-    #     function_or_method.__dict__["__widget__"] = self
-    #
-    #     # Store reference to widget
-    #     nbtools.manager._py_funcs[self.origin + '|' + self.name + '|widget'] = self
-    #
-    # @staticmethod
-    # def _determine_origin(function_or_method):
-    #     """Use the library name for the origin, or "Notebook" for tools defined in a notebook"""
-    #     return "Notebook" if function_or_method.__module__ == '__main__' else function_or_method.__module__
-    #
-    # @staticmethod
-    # def _safe_type(raw_type):
-    #     """Ensure that the provided type is a known type, defaulting to text"""
-    #     if raw_type == 'text' or raw_type == 'number' or raw_type == 'password' or raw_type == 'choice' or raw_type == 'file':
-    #         return raw_type
-    #     else:
-    #         return 'text'
-    #
-    # @staticmethod
-    # def _apply_custom_parameter_info(params, metadata):
-    #     # If output_var is overridden as a parameter, add to the parameters list as a special case
-    #     if 'output_var' in metadata and not any(p['name'] == 'output_var' for p in params):
-    #         params.append({
-    #             "name": 'output_var',
-    #             "label": 'output_variable',
-    #             "optional": True,
-    #             "default": '',
-    #             "description": 'The returned value of the function will be assigned to this variable, if provided.',
-    #             "hide": False,
-    #             "type": 'text',
-    #             "kinds": None,
-    #             "choices": [],
-    #             "id": None,
-    #             "events": None
-    #         })
-    #
-    #     # Iterate through each parameters in the function
-    #     for param in params:  # Iterate through each parameter
-    #         if param['name'] in metadata:  # If there is something to override
-    #             p_meta = metadata[param['name']]
-    #
-    #             # Handle overriding the display name
-    #             if 'name' in p_meta:
-    #                 param['label'] = p_meta['name']
-    #
-    #             # Handle overriding the description
-    #             if 'description' in p_meta:
-    #                 param['description'] = p_meta['description']
-    #
-    #             # Handle overriding the default value
-    #             if 'default' in p_meta:
-    #                 param['default'] = UIBuilder._safe_default(p_meta['default'])
-    #
-    #             # Handle hiding the parameter
-    #             if 'hide' in p_meta:
-    #                 param['hide'] = p_meta['hide']
-    #
-    #             # Handle optional parameters
-    #             if 'optional' in p_meta:
-    #                 param['optional'] = p_meta['optional']
-    #
-    #             # Handle specifying the parameter's type
-    #             if 'type' in p_meta:
-    #                 param['type'] = UIBuilder._safe_type(p_meta['type'])
-    #
-    #             # Handle specifying the parameter's accepted kinds
-    #             if 'kinds' in p_meta:
-    #                 param['kinds'] = p_meta['kinds']
-    #
-    #             # Handle giving the parameter a list of choices
-    #             if 'choices' in p_meta:
-    #                 param['choices'] = p_meta['choices']
-    #
-    #             # Handle assigning the parameter an ID
-    #             if 'id' in p_meta:
-    #                 param['id'] = p_meta['id']
-    #
-    #             # Handle giving the parameter a dict of events
-    #             if 'events' in p_meta:
-    #                 param['events'] = p_meta['events']
-    #
-    # @staticmethod
-    # def _docstring(function_or_method):
-    #     """Read docstring and protect against None"""
-    #     docstring = inspect.getdoc(function_or_method)
-    #     if docstring is None:
-    #         docstring = ''
-    #     return docstring
-    #
-    # @staticmethod
-    # def _is_primitive(thing):
-    #     """Determine if the value is a primitive"""
-    #     primitive = (int, str, bool, float)
-    #     return isinstance(thing, primitive)
-    #
-    # @staticmethod
-    # def _safe_default(default):
-    #     """If not safe to serialize in a traitlet, cast to a string"""
-    #
-    #     # If the value is not a primitive, cast to string
-    #     if not UIBuilder._is_primitive(default):
-    #         default = str(default)
-    #     return default
-    #
-    # @staticmethod
-    # def _guess_type(val):
-    #     """Guess the input type of the parameter based off the default value, if unknown use text"""
-    #     if isinstance(val, bool):
-    #         return "choice"
-    #     elif isinstance(val, int):
-    #         return "number"
-    #     elif isinstance(val, float):
-    #         return "number"
-    #     elif isinstance(val, str):
-    #         return "text"
-    #     elif hasattr(val, 'read'):
-    #         return "file"
-    #     else:
-    #         return "text"
-    #
-    # @staticmethod
-    # def _params(sig):
-    #     """Read params, values and annotations from the signature"""
-    #     params = []
-    #     for p in sig.parameters:
-    #         param = sig.parameters[p]
-    #         optional = param.default != inspect.Signature.empty
-    #         default = UIBuilder._safe_default(param.default) if param.default != inspect.Signature.empty else ''
-    #         annotation = param.annotation if param.annotation != inspect.Signature.empty else ''
-    #         type = UIBuilder._guess_type(default)
-    #
-    #         # Create the parameter attribute dict
-    #         p_attr = {
-    #             "name": param.name,
-    #             "label": param.name,
-    #             "optional": optional,
-    #             "default": default,
-    #             "description": annotation,
-    #             "hide": False,
-    #             "type": type,
-    #             "kinds": None,
-    #             "choices": [],
-    #             "id": None,
-    #             "events": None
-    #         }
-    #
-    #         # Special choices handling for boolean parameters
-    #         if isinstance(default, bool):
-    #             p_attr['choices'] = {
-    #                 'True': 'true',
-    #                 'False': 'false'
-    #             }
-    #
-    #         # Append it to the list
-    #         params.append(p_attr)
-    #     return params
-    #
-    # def _import(self, func):
-    #     """Return the namespace path to the function"""
-    #     # # If defined in notebook / main script, return function name
-    #     # if func.__module__ == "__main__":
-    #     #     return func.__name__
-    #     #
-    #     # # If defined in a module and the module name is in globals() return module.function()
-    #     # if func.__module__ in globals():  # WARNING: globals() may be bugged in IPython
-    #     #     return func.__module__ + "." + func.__name__
-    #     #
-    #     # # If module is not in globals(), assume the function was imported directly
-    #     # return func.__name__
-    #
-    #     return f'nbtools.tool(id="{self.name}", origin="{self.origin}").function_or_method'
