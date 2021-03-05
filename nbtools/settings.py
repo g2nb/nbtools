1234f039dd8f65480f7e84c2694c686371ba73f5
tmtabor <tmtabor@cloud.ucsd.edu>
2021-03-04 17:09:49 -0800

    Load tool packages from the config

--- nbtools/settings.py
+++ nbtools/settings.py
@@ -1,22 +1,44 @@
 import os
 import json
 import logging
-from pathlib import Path
+import jupyter_core.paths
 from IPython import get_ipython
 
 
 def load_settings():
-    """Attempt to load the nbtools settings file, fall back to default if not loaded"""
-    try:
-        with open(os.path.join(str(Path.home()), '.ipython', 'nbtools.json'), 'r') as settings_file:
-            return json.load(settings_file)
-    except FileNotFoundError as e:
-        logging.debug(f'nbtools setting file not found: {e}')
-    except json.JSONDecodeError as e:
-        logging.debug(f'unable to parse nbtools setting file: {e}')
+    """Attempt to load the nbtools settings files, fall back to default if not available"""
+    load = []
+    for p in jupyter_core.paths.jupyter_path():                                         # Get Jupyter data paths
+        nbtools_path = os.path.join(p, 'nbtools')
+        if os.path.exists(nbtools_path) and os.path.isdir(nbtools_path):                # Check for nbtools config
+            json_files = [j for j in os.listdir(nbtools_path) if j.endswith('.json')]   # Check for json in config dir
+            for jf in json_files:                                                       # Loop over json files
+                try:
+                    with open(os.path.join(nbtools_path, jf)) as json_file:             # Load and parse
+                        data = json.load(json_file)
+                        if 'load' in data and type(data['load']) is list:               # Ensure correct json format
+                            load += data['load']                                        # Add packages to load list
+                except FileNotFoundError as e:
+                    logging.debug(f'nbtools setting file not found: {e}')
+                except json.JSONDecodeError as e:
+                    logging.debug(f'unable to parse nbtools setting file: {e}')
+
+    # If packages were read, return the list to load
+    if len(load): return {"load": list(set(load))}
+
+    ############
+    # OLD WAY - REMOVE ONCE THE NEW WAY IS MORE PROVEN
+    # try:
+    #     with open(os.path.join(str(Path.home()), '.ipython', 'nbtools.json'), 'r') as settings_file:
+    #         return json.load(settings_file)
+    # except FileNotFoundError as e:
+    #     logging.debug(f'nbtools setting file not found: {e}')
+    # except json.JSONDecodeError as e:
+    #     logging.debug(f'unable to parse nbtools setting file: {e}')
+    ############
 
     # If it couldn't be loaded, return the default settings
-    return {"load": ["nbtools", "genepattern"]}  # FIXME: Special case for genepattern should be removed
+    else: return {"load": ["nbtools"]}
 
 
 def import_defaults():
