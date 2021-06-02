ceb9e663ea5721f3dd1b397f42a4a7f015c33323
tmtabor <tmtabor@cloud.ucsd.edu>
2021-06-02 10:54:43 -0700

    Removing old comment

--- src/version.ts
+++ src/version.ts
@@ -1,8 +1,6 @@
 const data = require('../package.json');
 
 /**
- * The _model_module_version/_view_module_version this package implements.
- *
  * The html widget manager assumes that this is the same as the npm package
  * version number.
  */
