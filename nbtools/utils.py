35f3127aaecd1c747325f9e8075a4054b05db1f0
tmtabor <tmtabor@cloud.ucsd.edu>
2024-05-07 10:17:33 -0700

    Refactoring is_url()

--- nbtools/utils.py
+++ nbtools/utils.py
@@ -5,6 +5,17 @@ import requests
 import threading
 
 
+def is_url(path_or_url):
+    matches_url = re.compile(
+        r'^(?:http|ftp)s?://'  # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+        r'localhost|'  # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    return re.match(matches_url, path_or_url)
+
+
 def open(path_or_url):
     """
     Wrapper for opening an IO object to a local file or URL
@@ -12,15 +23,7 @@ def open(path_or_url):
     :param path_or_url:
     :return:
     """
-    is_url = re.compile(
-        r'^(?:http|ftp)s?://'  # http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
-        r'localhost|'  # localhost...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
-        r'(?::\d+)?'  # optional port
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-
-    if re.match(is_url, path_or_url):
+    if is_url(path_or_url):
         return urllib.request.urlopen(path_or_url)
     else:
         return builtins.open(path_or_url)
